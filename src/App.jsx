7881e553e9b3ee17daca0af18a0fa2413f9d702b
talrejah02 <talrejah02@gmail.com>
2022-04-05 04:01:04 +0530

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Nav from "./components/Navbar/Nav";
 import {Routes,Route} from "react-router-dom"
 import Home from "./pages/Home page/Home";
+import {Login} from "../src/pages/index"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/Login" element={<Login/>}/>
       </Routes>
     </div>
   );
--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -34,7 +34,7 @@ function Nav() {
             </section>
             <section className="nav-right">
                 <NavLink to="/" className="nav-link">Shop</NavLink>
-                <NavLink to="/" className="nav-link">Login</NavLink>
+                <NavLink to="/Login" className="nav-link">Login</NavLink>
                 <NavLink to="/" className="nav-link">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
                         aria-hidden="true" role="img" className="iconify iconify--ph" width="20" height="20"
