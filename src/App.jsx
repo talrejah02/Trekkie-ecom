1cfa0441cc43df76c05d03619bdc89860650f3cc
talrejah02 <talrejah02@gmail.com>
2022-04-13 19:55:56 +0530

    Feat:added products Refactor: Pages CSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Routes,Route} from "react-router-dom"
 
 
 import {Home,ProductPage,Login,Cart} from "./pages/index";
+import MockmanEs from "mockman-js";
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route path="/Login" element={<Login/>}/>
         <Route path="/Shop" element={<ProductPage/>}/>
         <Route path="/Cart" element={<Cart/>}/>
-
+        <Route path="/mock"element={<MockmanEs/>}/>
       </Routes>
     </div>
   );
