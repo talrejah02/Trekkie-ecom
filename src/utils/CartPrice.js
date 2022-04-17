829f1a8058fee81926e26ef25b1d3cb698d7de6c
talrejah02 <talrejah02@gmail.com>
2022-04-18 00:36:33 +0530

    feat:added cart total

--- src/utils/CartPrice.js
+++ src/utils/CartPrice.js
@@ -1,15 +1,16 @@
 export const cartInitialprice=(cart)=>{
  return cart.reduce((acc,curr)=>{
         return acc+curr.orignalPrice*curr.quantity
-    })
+    },0)
 }
+
 export const cartDiscountprice=(cart)=>{
     return cart.reduce((acc,curr)=>{
         return acc+(curr.orignalPrice-curr.discountedPrice)*curr.quantity
-    })
+    },0)
 }
 export const cartTotalprice=(cart)=>{
    return cart.reduce((acc,curr)=>{
        return acc+curr.discountedPrice*curr.quantity
-   })
+   },0)
 }
